238029cc8055dd144c4c1f31f38e3abf0631dc44
ptnkjke <ptnkjke@gmail.com>
2014-07-24 21:42:26 +0400

    add vk request

--- src/net/ptnkjke/Testing.java
+++ src/net/ptnkjke/Testing.java
@@ -12,8 +12,12 @@ public class Testing {
         Map<String, String> parameters = new HashMap<String, String>();
         parameters.put("data","привет");
 
-        String response = Utils.executePost("http://localhost:8080/data",parameters);
+        //String response = Utils.executePost("http://localhost:8080/data",parameters);
 
-        response = Utils.executeGet("http://localhost:8080/data",parameters);
+        //response = Utils.executeGet("http://localhost:8080/data",parameters);
+
+        String result = Utils.executeVK("https://api.vk.com/method/photos.search?lat=30.0&long=30.0&count=100&radius=50000&v=5.23&offset=3000");
+
+        System.out.println(result);
     }
 }
--- src/net/ptnkjke/Utils.java
+++ src/net/ptnkjke/Utils.java
@@ -145,4 +145,36 @@ public class Utils {
         }
     }
 
+    public static String executeVK(String urle){
+        try {
+            URL url = new URL(urle);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-length", "0");
+            con.setConnectTimeout(300000);
+
+            con.connect();
+
+            int resp = con.getResponseCode();
+            if(resp == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String line;
+                StringBuilder sb = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+                br.close();
+
+                return sb.toString();
+            } else {
+
+            }
+
+        } catch(Exception e) { e.printStackTrace(); }
+
+        return null;
+    }
 }
